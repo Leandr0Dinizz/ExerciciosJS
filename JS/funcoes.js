722381585c658dbb9210bc903c69a1eb1e29905e
LEANDRO DINIZ MENEZES <leandro.dmenezes@senacsp.edu.br>
2024-04-09 21:59:00 -0300

    Segundo Commit

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -190,7 +190,7 @@ function posNeg(){
 }//fim do método
 
 
-function teDez(){
+function exercicio1(){
     var res =0;
     var res = "Números: "
 }
@@ -200,7 +200,7 @@ for(i = 1; i <= 10; i++){
 document.getElementById("resultado").innerHTML = res
 //fim do método}
 
-function parVinte(){
+function exercicio2(){
     var res;
     var res = "Números pares";
 
@@ -213,7 +213,7 @@ function parVinte(){
 }//fim do método
 
 
-function calCem(){
+function exercicio3(){
     var res = 0;
 
 
@@ -223,3 +223,181 @@ function calCem(){
     }
     document.getElementById("resultado").innerHTML = res
 }//fim do método
+
+function exercicio4(){
+    var vet = [];
+    var posicao = 0;
+ 
+    for(i = 1; i < 51; i++){
+        if(i % 5 == 0){
+            vet[posicao] = i;
+            posicao += 1;
+        }
+    }
+    document.getElementById("resultado").innerHTML = vet;
+}//fim do método
+
+function exercicio5() {
+    var numero = parseInt(document.getElementById("tpNum").value);
+    var res = "Números de 1 até " + numero + ":";
+ 
+    for (var i = 1; i <= numero; i++) {
+        res += "\n" + i;
+    }
+    document.getElementById("resultado").innerHTML = res;
+}//fim do método
+
+function exercicio6() {
+    var numero = parseInt(document.getElementById("tpNum").value);
+    var soma = 0;
+ 
+    for (var i = 1; i <= numero; i++) {
+        soma += i;
+    }
+    document.getElementById("resultado").innerHTML = "A soma dos números de 1 até " + numero + " é: " + soma;
+}//fim do método
+
+function exercicio7() {
+    var res = "Números primos de 1 a 20: ";
+ 
+    for (var i = 2; i <= 20; i++) {
+        if (isPrimo(i)) {
+            res += i + " ";
+        }
+    }
+    document.getElementById("resultado").innerHTML = res;
+}
+ 
+function isPrimo(numero) {
+    for (var j = 2; j < numero; j++) {
+        if (numero % j === 0) {
+            return false;
+        }
+    }
+    return numero !== 1;
+}//fim método
+
+function exercicio8() {
+    var numero = parseInt(document.getElementById("tpNum").value);
+    var divisores = 0;
+   
+    for(var i = 1; i <= numero; i++) {
+        if (numero % i == 0) {
+            divisores++;
+        }
+    }    
+    
+    if (divisores == 2) {
+        document.getElementById('resultado').innerHTML = 'É primo';
+    } else {
+        document.getElementById('resultado').innerHTML = 'Não é primo';
+    }
+}//fim do método
+
+function exercicio9(){
+    var numero = parseInt(document.getElementById('tpNum').value);
+    var resultado = 1;
+  
+    for(var i = 1 ; i<=numero ; i++)
+     resultado *= i;
+  
+     document.getElementById('resultado').innerHTML = resultado;
+}//fim do método
+
+function exercicio10(){
+    var fibonacci = [];
+    fibonacci[0] = 1;
+    fibonacci[1] = 1;
+
+    for(i = 2; i < 10; i++){
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+    }
+
+    document.getElementById("resultado").innerHTML = fibonacci;
+}//Fim do método
+
+function exercicio11(){
+    var num;
+    var numPar = [];
+    var numImpar = [];
+    var posicaoPar = 0;
+    var posicaoImpar = 0;
+
+    num = parseInt(document.getElementById("tpNum").value)
+
+   
+    for(i = 1; i < num + 1; i++){
+        if(i == 1){
+            numImpar[0] = i;
+            posicaoImpar = 1;
+        }
+        else if(i % 2 == 0){
+            numPar[posicaoPar] = i;
+            posicaoPar += 1;
+        }
+        else{
+            numImpar[posicaoImpar] = i;
+            posicaoImpar += 1;
+        }
+    }
+
+    if(num < 1){
+        res = "Erro!! informe um valor maior que 01"
+        document.getElementById("resultado").innerHTML = res
+    }
+    else{
+        document.getElementById("resultado").innerHTML = "Número Pares: \n" + numPar + "\n Número Ímpares: \n" + numImpar;
+    }
+}//Fim do método
+
+function exercicio12(){
+    var num;
+    var primo = [];
+    var posicao = 0;
+
+    num = parseInt(document.getElementById("tpNum").value)
+
+    for(i = 1; i < num + 1; i++){
+        
+        if(i == 2 || i == 3 || i == 5 || i == 7){
+            primo[posicao] = i;
+        }
+        else if(num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0){
+            continue
+        }
+        else{
+            primo[posicao] = i;
+        }
+        posicao++
+    }
+
+    if(num < 2){
+        document.getElementById("resultado").innerHTML = "ERRO!!! Informe um número maior que 1";
+    }
+    else{
+        document.getElementById("resultado").innerHTML = primo;
+    }
+    
+}//Fim do método
+
+
+function exercicio13(){
+    var num;
+    var divisores = 0;
+
+    num = parseInt(document.getElementById("tpNum").value)
+
+    for(i = 0; i < num; i++){
+        if(num % i == 0){
+            divisores += i;
+        }
+    }
+
+    if(divisores == num){
+        document.getElementById("resultado").innerHTML = "Número Perfeito"
+    }
+    else{
+        document.getElementById("resultado").innerHTML = "Não é número perfeito"
+    }
+    
+}//Fim do método
